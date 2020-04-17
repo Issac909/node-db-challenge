c949c425155e2e212776ef87b1a5d2d9a8a3d201
Issac Moreno <issaxavier909@gmail.com>
2020-04-17 10:01:12 -0700

    Knex table schema made(before migrations)

---
+++ data/migrations/20200417085124_projects.js
@@ -0,0 +1,44 @@
+exports.up = function (knex) {
+  return knex.schema
+  .createTable('projects', (project) => {
+    project.increments();
+    project.text('name', 128).notNullable();
+    project.text('description');
+    project.boolean('completed').defaultTo(false);
+  })
+  .createTable('resources', resource => {
+      resource.increments();
+      resource.string('name', 128).notNullable().unique();
+      resource.text('description');   
+  })
+  .createTable('tasks', task => {
+      task.increments().unique();
+      task.string('description', 128).notNullable()
+      task.text('notes');
+      task.boolean('completed').defaultTo(false);
+      task
+        .integer('project_id').notNullable()
+        .references('id').inTable('projects')
+        .onDelete('CASCADE').onUpdate('CASCADE');
+  })
+  .createTable('project-resources', tbl => {
+      tbl
+        .integer('project_id').notNullable()
+        .inTable('projects').references('id')
+        .onDelete('CASCADE').onUpdate('CASCADE');
+      tbl
+        .integer('resource_id').notNullable()
+        .references('id').inTable('resources')
+        .onDelete('CASCADE').onUpdate('CASCADE');
+      tbl
+        .primary(['project_id', 'resource_id']);
+  });
+};
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('project-resources')
+        .dropTableIfExists('projects')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+};
--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,8 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
+    client: 'sqlite3', 
     connection: {
       filename: './data/projects.db3'
     },
@@ -15,37 +13,5 @@ module.exports = {
       directory: './data/seeds'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+  useNullAsDefault: true
 };
